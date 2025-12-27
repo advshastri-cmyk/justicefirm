32a9ce0bc14c1d4e0caa2ed816d940bcfbc557d0
advshastri-cmyk <adv.shastri@gmail.com>
2025-12-27 12:39:25 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'justice-firms-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon-192x192.png',
+  '/favicon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetching content
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
